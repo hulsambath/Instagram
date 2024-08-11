df1b5cdb62de390bb59c641342eed67f1691a79f
Hul Sambath <hulsambth14@gmail.com>
2024-08-11 13:49:00 +0700

    Close #8 firebase setup

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,14 +1,16 @@
 import 'package:api_repository/api_repository.dart';
 import 'package:instagram/app/view/app.dart';
 import 'package:instagram/bootstrap.dart';
+import 'package:instagram/firebase_options_stg.dart';
 
 void main() {
   const apiRepository = ApiRepository();
-    
+
   bootstrap(
     (powerSyncRepository) {
       return const App(apiRepository: apiRepository);
     },
-    env: EnvDev,
+    options: DefaultFirebaseOptions.currentPlatform,
+    env: null,
   );
 }
